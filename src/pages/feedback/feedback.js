81ffa2bdffd49abc77c6ef31916e6de16b6bc4b1
habi <habibul.rahman@lndata.com>
2022-08-14 18:08:02 +0700

    feat: :sparkles: Init MUI Components

---
+++ src/pages/feedback/feedback.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./feedback.css";
+
+export default function feedback() {
+  return (
+    <div className="feedback">
+      <h1 className="textRow">Coming Soon</h1>
+    </div>
+  );
+}

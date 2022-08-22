2a445dbf9d64796bf5ad2cd6d9b013fd83c55391
habi <habibul.rahman@lndata.com>
2022-08-22 09:42:57 +0700

    feat: :sparkles: Update Drawer Styling based on Figma

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,24 +1,24 @@
 import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
+import "./navigation.css";
+import { Menu, Rekonstruksi, Segmentasi } from "./drawerData";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import CssBaseline from "@mui/material/CssBaseline";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
+// import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import { Outlet } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { Link, Outlet } from "react-router-dom";
+import logo from "../../assets/horizontal-white.png";
 
 const drawerWidth = 240;
 
@@ -64,92 +64,147 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
+  justifyContent: "center",
 }));
 
 export default function PersistentDrawerLeft() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
 
   const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
+    setOpen(!open);
   };
 
   return (
     <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: "none" }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            3DVT
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: drawerWidth,
-            boxSizing: "border-box",
-          },
+      <IconContext.Provider
+        value={{
+          className: "iconContext",
         }}
-        variant="persistent"
-        anchor="left"
-        open={open}
       >
-        <DrawerHeader>
-          <Typography align="center" noWrap component="div">
-            Main Menu
-          </Typography>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "ltr" ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          open={open}
+          sx={{ backgroundColor: "#FFFFFF" }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, color: "#2A3042" }}
+              // sx={{ mr: 2, ...(open && { display: "none" }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+              backgroundColor: "#2A3042",
+              color: "white",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <img src={logo} alt="3DVT" width="100"></img>
+          </DrawerHeader>
+          <Divider
+            variant="string"
+            textAlign="left"
+            sx={{ pt: 2, color: "#a6b0cf" }}
+          >
+            Menu
+          </Divider>
+          <List>
+            {Menu.map((item, index) => {
+              return (
+                <ListItem key={index} className={item.cName} disablePadding>
+                  <ListItemButton>
+                    <Link to={item.path}>
+                      <ListItemIcon
+                        sx={{
+                          minWidth: 0,
+                          pl: 2,
+                          pr: 2,
+                        }}
+                      >
+                        {item.icon}
+                      </ListItemIcon>
+                      <ListItemText primary={item.title} />
+                    </Link>
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </List>
+          <Divider
+            variant="string"
+            textAlign="left"
+            sx={{ pt: 2, color: "#a6b0cf" }}
+          >
+            Segmentasi
+          </Divider>
+          <List>
+            {Segmentasi.map((item, index) => {
+              return (
+                <ListItem key={index} className={item.cName} disablePadding>
+                  <ListItemButton>
+                    <Link to={item.path}>
+                      <ListItemIcon
+                        sx={{
+                          minWidth: 0,
+                          pl: 2,
+                          pr: 2,
+                        }}
+                      >
+                        {item.icon}
+                      </ListItemIcon>
+                      <ListItemText primary={item.title} />
+                    </Link>
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </List>
+          <Divider
+            variant="string"
+            textAlign="left"
+            sx={{ pt: 2, color: "#a6b0cf" }}
+          >
+            Rekonstruksi
+          </Divider>
+          {Rekonstruksi.map((item, index) => {
+            return (
+              <ListItem key={index} className={item.cName} disablePadding>
+                <ListItemButton>
+                  <Link to={item.path}>
+                    <ListItemIcon
+                      sx={{
+                        minWidth: 0,
+                        pl: 2,
+                        pr: 2,
+                      }}
+                    >
+                      {item.icon}
+                    </ListItemIcon>
+                    <ListItemText primary={item.title} />
+                  </Link>
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </Drawer>
+      </IconContext.Provider>
       <Main open={open}>
         <DrawerHeader />
         <Outlet />
